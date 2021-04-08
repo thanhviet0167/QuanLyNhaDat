409f3baf432ed59cdeccb819e1e68b368f0708b7
Add file <18126037@student.hcmus.edu.vn>
2021-04-08 18:07:26 +0700

    Thiet ke giao dien khach hang va chu nha

---
+++ QuanLyNhaDat/QuanLyNhaDat/BaiDang.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaDat
+{
+    public partial class BaiDang : Form
+    {
+        public BaiDang()
+        {
+            InitializeComponent();
+            ThongTin.Visible = false;
+            FormBaiDang.Visible = false;
+            DataLoad.Visible = false;
+        }
+        SqlConnection sqlCon = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataLoad.Visible = false;
+            FormBaiDang.Visible = false;
+            ThongTin.Visible = true;
+            
+        }
+
+       
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DataLoad.Visible = false;
+            ThongTin.Visible = false;
+            FormBaiDang.Visible = true;
+           
+        }
+
+        private void BaiDang_Load(object sender, EventArgs e)
+        {
+            txtMaChuNha.Enabled = false;
+            txtHoTen.Enabled = false;
+            txtSdt.Enabled = false;
+            txtAdress.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ThongTin.Visible = false;
+            FormBaiDang.Visible = false;
+            DataLoad.Visible = false;
+            txtDate.Clear();
+            txtMaChuNha.Clear();
+            txtMaNha.Clear();
+        
+         
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if(txtMCN.Text != "" && txtMaNha.Text != "" && txtDate.Text != "" && (radioButton1.Checked || radioButton2.Checked))
+            {
+                String strConnect = @"Data Source=DESKTOP-EIVACRQ\SQLEXPRESS;Initial Catalog=QuanLyNhaDat;Persist Security Info=True;User ID=ChuNha;Password=B";
+
+                sqlCon = new SqlConnection(strConnect);
+                sqlCon.Open();
+
+                string sqlInsert = "INSERT INTO BAI_DANG VALUES (@Loai, @MaNha, @MaChuNha, @NgayDang)";
+                SqlCommand cmd = new SqlCommand(sqlInsert, sqlCon);
+                if (radioButton2.Checked)
+                {
+                    cmd.Parameters.AddWithValue("Loai", false);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("Loai", true);
+                }
+                cmd.Parameters.AddWithValue("MaNha", txtMaNha.Text);
+                cmd.Parameters.AddWithValue("MaChuNha", txtMCN.Text);
+                cmd.Parameters.AddWithValue("NgayDang", txtDate.Text);
+                txtDate.Clear();
+                txtMaChuNha.Clear();
+                txtMaNha.Clear();
+                MessageBox.Show("Tao bai dang thanh cong");
+                cmd.ExecuteNonQuery();
+
+                sqlCon.Close();
+            }
+            else
+            {
+                if(txtDate.Text == "")
+                {
+                    ErrorDate.Text =  "Ngay dang ki trong";
+                }
+                else
+                {
+                    ErrorDate.Clear();
+                }
+
+                if (txtMaNha.Text == "")
+                {
+                    ErrorMaNha.Text = "Ma nha trong";
+                }
+                else
+                {
+                    String strConnect = @"Data Source=DESKTOP-EIVACRQ\SQLEXPRESS;Initial Catalog=QuanLyNhaDat;Persist Security Info=True;User ID=ChuNha;Password=B";
+
+                    sqlCon = new SqlConnection(strConnect);
+                    sqlCon.Open();
+
+                    string sqlSelect = "SELECT * FROM BAI_DANG";
+                    SqlCommand cmd = new SqlCommand(sqlSelect, sqlCon);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    Boolean b = true;
+                    while(dr.Read())
+                    {
+
+                        if (Convert.ToString(dr["MaNha"]).Equals(txtMaNha.Text))
+                        {
+                            b = false;
+                        }
+                    }
+                    if(b)
+                    {
+                        ErrorMaNha.Clear();
+                    }
+                    else
+                    {
+                        ErrorMaNha.Text = "Ma nha da ton tai";
+                    }
+                    sqlCon.Close();
+
+                }
+                if (txtMaChuNha.Text == "")
+                {
+                    ErrorMCN.Text = "Ma chu nha trong";
+                }
+                else
+                {
+                    ErrorMCN.Clear();
+                }
+                if (!radioButton1.Checked && !radioButton2.Checked)
+                {
+                    ErrorDate.Text = "Loai bai dang chua duoc chon";
+                }
+                else
+                {
+                    ErrorLoai.Clear();
+                }
+            }
+            
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            radioButton2.Checked = false;
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            ThongTin.Visible = false;
+            FormBaiDang.Visible = false;
+            DataLoad.Visible = true;
+            txtDate.Clear();
+            txtMaChuNha.Clear();
+            txtMaNha.Clear();
+
+            
+            String strConnect = @"Data Source=DESKTOP-EIVACRQ\SQLEXPRESS;Initial Catalog=QuanLyNhaDat;Persist Security Info=True;User ID=ChuNha;Password=B";
+
+            sqlCon = new SqlConnection(strConnect);
+            sqlCon.Open();
+
+            string sqlSelect = "SELECT * FROM BAI_DANG";
+            SqlCommand cmd = new SqlCommand(sqlSelect, sqlCon);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+            sqlCon.Close();
+        }
+    }
+}
---
+++ QuanLyNhaDat/QuanLyNhaDat/TimKiemNha.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaDat
+{
+    public partial class TimKiemNha : Form
+    {
+        public TimKiemNha()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlCon = null;
+
+        private void GetAll()
+        {
+            String strConnect = @"Data Source=DESKTOP-EIVACRQ\SQLEXPRESS;Initial Catalog=QuanLyNhaDat;Persist Security Info=True;User ID=KhachHang;Password=A";
+
+            sqlCon = new SqlConnection(strConnect);
+            sqlCon.Open();
+
+            string sqlSelect = "SELECT * FROM NHA Where TinhTrang=0";
+            SqlCommand cmd = new SqlCommand(sqlSelect, sqlCon);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["MaNha"].Visible = false;
+            dataGridView1.Columns["MaChuNha"].Visible = false;
+            dataGridView1.Columns["MaLN"].Visible = false;
+            dataGridView1.Columns["MaNV"].Visible = false;
+            dataGridView1.Columns["MaCN"].Visible = false;
+
+            sqlCon.Close();
+        }
+        private void TimKiemNha_Load(object sender, EventArgs e)
+        {
+            List<string> listprice= new List<string>() {"Duoi 12 tr" ,"Tu 12tr đen 25tr", "Tren 25tr"};
+            comboBoxGiaNha.DataSource = listprice;
+            List<string> listtype = new List<string>() { "Loai 1", "Loai 2", "Loai 3","Loai 4" };
+            comboBoxLoaiNha.DataSource = listtype;
+            List<string> address = new List<string>() { "Quan 1", "Quan 2", "Quan 3", "Quan 4" , "Quan 5", "Quan 7", "Quan 8", "Quan 10"};
+            comboBoxKhuVuc.DataSource = address;
+            this.GetAll();
+        //    dataGridView1.Visible = false;
+
+        
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private Boolean check_GiaNha(double gia, string str)
+        {
+            if(str == "Duoi 12 tr")
+            {
+                if(gia < 12000000)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (str == "Tu 12tr đen 25tr")
+                {
+                    if (gia >= 12000000 && gia <= 25000000)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (str == "Tren 25tr")
+                    {
+                        if (gia > 25000000)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private Boolean check_khuvuc(string khuvuc, string str)
+        {
+            string convert = khuvuc.ToLower();
+            string str_convert = str.ToLower();
+            if(str == "Quan 1" || str == "Quan2" || str == "Quan 3" || str == "Quan 4" || str == "Quan 5" ||
+                str == "Quan 7" || str == "Quan 8" || str == "Quan 10")
+            {
+                if (convert.Contains(str_convert))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private Boolean check_LoaiNha(string MaLN, string str)
+        {
+            if(str == "Loai 1")
+            {
+                if(MaLN == "LN001")
+                {
+                    return true;
+                }
+            }
+            if (str == "Loai 2")
+            {
+                if (MaLN == "LN002")
+                {
+                    return true;
+                }
+            }
+            if (str == "Loai 3")
+            {
+                if (MaLN == "LN003")
+                {
+                    return true;
+                }
+            }
+            if (str == "Loai 4")
+            {
+                if (MaLN == "LN004")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Boolean check_SoPhong(int sp, int sl)
+        {
+            if(sp == sl)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var index = new List<int>();
+
+            int check = 0;
+            if (comboBoxGiaNha.Text != "")
+            {
+                check += 1;
+            }
+            if(comboBoxKhuVuc.Text != "")
+            {
+                check += 1;
+            }
+            if(comboBoxLoaiNha.Text != "")
+            {
+                check += 1;
+            }
+            if(numericUpDownSoPhong.Value > 0)
+            {
+                check += 1;
+            }
+
+
+            String strConnect = @"Data Source=DESKTOP-EIVACRQ\SQLEXPRESS;Initial Catalog=QuanLyNhaDat;Persist Security Info=True;User ID=KhachHang;Password=A";
+
+            sqlCon = new SqlConnection(strConnect);
+            sqlCon.Open();
+
+            string sqlSelect = "SELECT * FROM NHA Where TinhTrang=0";
+            SqlCommand cmd = new SqlCommand(sqlSelect, sqlCon);
+            SqlDataReader dr = cmd.ExecuteReader();
+            int k = 0;
+            while (dr.Read())
+            {
+                int tmp_check = 0;
+                if (check_GiaNha(Convert.ToDouble(dr["TienThue"]), comboBoxGiaNha.Text))
+                {
+                    tmp_check += 1;
+                    Console.Out.WriteLine("Gia nha dung");
+                }
+                if (check_khuvuc(Convert.ToString(dr["DiaChi"]), comboBoxKhuVuc.Text))
+                {
+                    tmp_check += 1;
+                    Console.Out.WriteLine("Khu vuc dung");
+                }
+                if(check_LoaiNha(Convert.ToString(dr["MaLN"]), comboBoxLoaiNha.Text))
+                {
+                    tmp_check += 1;
+                    Console.Out.WriteLine("Ma loai nha dung");
+                }
+                var sp = numericUpDownSoPhong.Value;
+                if (check_SoPhong(Convert.ToInt32(dr["SoPhong"]), (int)sp))
+                {
+                    tmp_check += 1;
+                    Console.Out.WriteLine("So phong dung");
+                }
+                if(check <= tmp_check)
+                {
+
+                }
+                else
+                {
+                    index.Add(k);
+                }
+                k++;
+            }
+            this.GetAll();
+            CurrencyManager currencyManager1 = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+            currencyManager1.SuspendBinding();
+            for (var i = 0; i < index.Count; i++)
+            {
+                dataGridView1.Rows[index[i]].Visible = false;
+            }
+            currencyManager1.ResumeBinding();
+
+            sqlCon.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(dataGridView1.Columns[e.ColumnIndex].Name == "Mua")
+            {
+                if(MessageBox.Show("Ban muon mua ngoi nha nay?","Message",MessageBoxButtons.YesNo,MessageBoxIcon.Question)
+                    ==DialogResult.Yes)
+                {
+                    var index = dataGridView1.Columns[e.RowIndex].Index;
+                    
+                    Console.Out.WriteLine(dataGridView1.Rows[e.RowIndex].Cells["MaNha"].FormattedValue.ToString());
+                   
+
+                }
+
+            }
+            else
+            {
+                if (dataGridView1.Columns[e.ColumnIndex].Name == "ThueNha")
+                {
+                    if (MessageBox.Show("Ban muon thue ngoi nha nay?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                        == DialogResult.Yes)
+                    {
+                        var index = dataGridView1.Columns[e.RowIndex].Index;
+
+                        Console.Out.WriteLine(dataGridView1.Rows[e.RowIndex].Cells["MaNha"].FormattedValue.ToString());
+
+
+                    }
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new KhachHang().Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.GetAll();
+        }
+    }
+}
